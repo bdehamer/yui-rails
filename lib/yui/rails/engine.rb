98ecb308ef9f7b22ee1da4ec03114cadfa9c00c0
Brian DeHamer <brian.dehamer@hp.com>
2012-03-27 06:29:15 -0700

    Moving default ComboHandler middleware position to be after Rack::ETag

--- lib/yui/rails/engine.rb
+++ lib/yui/rails/engine.rb
@@ -4,7 +4,7 @@ module YUI
     # This engine class adds the gem's vendored assets to sprockets' load path
     class Engine < ::Rails::Engine
       initializer 'YUI combo handler' do |app|
-        app.config.middleware.insert_after ::ActionDispatch::Static,
+        app.config.middleware.insert_after ::Rack::ETag,
           ::YUI::Rails::ComboHandler
       end
     end
